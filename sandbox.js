0f040faefa6ac3cd05e2ff2752e0cc7334bff886
okedo <eliahmwelangi01@gmail.com>
2023-09-26 14:46:31 +0300

    quiz section

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,19 @@
 const correctAnswers = ['C', 'B', 'C', 'D', 'A'];
-const form = document.querySelector('quiz-form');
+const form = document.querySelector('.quiz-form');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+
+    let score = 0;
+    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+    // cheching the correct answer
+    userAnswer.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 20;
+        }
+    })
+    console.log(score);
 });
+
 
